e50b64684d587e500f21fad1fff2bfe2e0d1a4c1
surya1337gkrm <suryavenkatesh0@gmail.com>
2020-09-02 15:11:24 +0530

    Updates

---
+++ precache-manifest.510210e75004a5d6c30ce99b29e74b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed94385f1c02e3ea0d72fb133a1899dd",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "dca39b77370b258242e2",
+    "url": "/netflix-clone/static/css/main.b9e6524a.chunk.css"
+  },
+  {
+    "revision": "8da2d0dd7a6e5326c846",
+    "url": "/netflix-clone/static/js/2.633f2a4e.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.633f2a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca39b77370b258242e2",
+    "url": "/netflix-clone/static/js/main.00ba30d9.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
